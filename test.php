5b2d84d191934558e356fa8502f566a1534f3c0e
JohnPaul <testerzttest@gmail.com>
2019-04-04 11:23:12 +0530

    Changes in test Signed-off-by: JohnPaul <testerzttest@gmail.com>

--- test.php
+++ test.php
@@ -17,5 +17,20 @@
 </table>
 </form>
 </body>
+<script>
+    function isEmpty(str){
+        return !str.replace(/\s+/, '').length;
+    }
+    function getValid(){
 
+         var text=document.getElementById('textid').value;
+         if(isEmpty(text)){
+         document.getElementById("text_valid").innerHTML = "Enter Text";
+         document.getElementById("textid").focus();
+         }
+         else{
+         document.getElementById("textid").innerHTML = "";
+         }       
+    }
+</script>
 </html>
